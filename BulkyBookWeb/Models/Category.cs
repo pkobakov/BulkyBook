e44d3179809307ef2891d70d39663f353eae3987
Petar Kobakov <pkobakov@gmail.com>
2022-11-03 18:03:40 +0200

    Custom Property Validation

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -6,8 +7,11 @@ namespace BulkyBookWeb.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]  
+        [Required]
+        [MinLength(3, ErrorMessage ="Invalid Name. Category Name should content at least 3 characters.")]
         public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Invalid Display Order Value. It should be between 1 and 100.")]
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
