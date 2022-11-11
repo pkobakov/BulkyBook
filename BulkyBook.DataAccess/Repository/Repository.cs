a8684d35f30522c8f245f027b97bea9ebbc38f28
Petar Kobakov <pkobakov@gmail.com>
2022-11-11 16:59:08 +0200

    N Tier Architecture. Repository Pattern. Implementing IRepository

---
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFIrstOrDefault(Expression<Func<Task, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3, ErrorMessage = "Invalid Name. Category Name should content at least 3 characters.")]
+        public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Invalid Display Order Value. It should be between 1 and 100.")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    }
+}
