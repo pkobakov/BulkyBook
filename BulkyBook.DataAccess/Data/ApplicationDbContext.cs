5680911e10e851646a9cdeb3b70545969a97173c
Petar Kobakov <pkobakov@gmail.com>
2022-11-14 13:36:07 +0200

    Create Cover Type Model. Add-Migration AddCoverTypeModel

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace BulkyBook.DataAccess.Data
         }
 
         public DbSet<Category> Categories {get; set;}
+        public DbSet<CoverType> CoverTypes { get; set;}
 
 
     }
--- BulkyBook.DataAccess/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ BulkyBook.DataAccess/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -22,7 +22,7 @@ namespace BulkyBookWeb.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
 
-            modelBuilder.Entity("BulkyBookWeb.Models.Category", b =>
+            modelBuilder.Entity("BulkyBook.Models.Category", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
@@ -43,6 +43,24 @@ namespace BulkyBookWeb.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("BulkyBook.Models.CoverType", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("nvarchar(50)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("CoverTypes");
                 });
 #pragma warning restore 612, 618
         }
