600785c9a832901ab9fb3cdcb5243d97b1f44f82
Petar Kobakov <pkobakov@gmail.com>
2022-11-15 13:37:02 +0200

    Adding CoverType Repository

---
+++ BulkyBook.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;   
+        }
+
+        public void Update(CoverType obj)
+        {
+           _db.CoverTypes.Update(obj);
+        }
+    }
+}
