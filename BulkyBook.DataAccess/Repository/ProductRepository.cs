a50265cb4f45ee29a911e2d290ceabe7f6a0d8ca
Petar Kobakov <pkobakov@gmail.com>
2022-11-15 13:37:42 +0200

    Setting the Product Repository

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;   
+        }
+
+        public void Update(Product obj)
+        {
+           //_db.Products.Update(obj);
+           var productToUpdate = _db.Products.FirstOrDefault( p => p.Id == obj.Id );
+            if ( productToUpdate != null )
+            {
+                productToUpdate.Id = obj.Id;
+                productToUpdate.Title= obj.Title;
+                productToUpdate.Description= obj.Description;
+                productToUpdate.CategoryId= obj.CategoryId;
+                productToUpdate.CoverTypeId= obj.CoverTypeId;
+                productToUpdate.Author= obj.Author;
+                productToUpdate.ISBN= obj.ISBN; 
+                productToUpdate.ListPrice = obj.ListPrice;
+                productToUpdate.Price = obj.Price;
+                productToUpdate.Price50 = obj.Price50;  
+                productToUpdate.Price100= obj.Price100;
+
+                if (productToUpdate.ImageURL != null)
+                {
+                    productToUpdate.ImageURL = obj.ImageURL;
+                }
+
+            }
+
+        }
+    }
+}
