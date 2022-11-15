d4747a3c4b39fce34c57a0c8f2f62e6ebccb9f12
Petar Kobakov <pkobakov@gmail.com>
2022-11-15 12:58:06 +0200

    Adding CoverType.cs

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace BulkyBook.DataAccess.Data
 
         public DbSet<Category> Categories {get; set;}
         public DbSet<CoverType> CoverTypes { get; set;}
+        public DbSet<Product> Products { get; set; }
 
 
     }
