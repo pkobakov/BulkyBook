90d45c8e543191d0b404bc1bb10d390c294f1751
Petar Kobakov <pkobakov@gmail.com>
2022-12-15 15:55:06 +0200

    Loading ShoppingCart List in the Get Action

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -27,9 +27,11 @@ namespace BulkyBook.DataAccess.Repository
            dbSet.Add(entity);
         }
         //includeProp - "Category, CoverType"
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+
             if (includeProperties != null)
             {
                 foreach (var property in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
